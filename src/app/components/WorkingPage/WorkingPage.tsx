30a5a7e09b53dbf53b8d95fd6e6f1db777399770
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-03-18 19:35:23 +0600

    working service page design

--- src/app/components/WorkingPage/WorkingPage.tsx
+++ src/app/components/WorkingPage/WorkingPage.tsx
@@ -8,6 +8,9 @@ const WorkingPage = () => {
         <div className="w-12 h-12 border-8 border-dashed rounded-full animate-spin mt-5 border-blue-600"></div>
         <p className="text-7xl font-thin">rking on this service...</p>
       </div>
+      <div>
+        <p>Wait for next update...</p>
+      </div>
       <Link href="/">
         <button className="py-2 px-3 bg-blue-600 text-slate-50">
           Back to Home
